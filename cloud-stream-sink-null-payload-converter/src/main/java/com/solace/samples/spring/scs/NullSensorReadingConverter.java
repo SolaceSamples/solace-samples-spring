d7d1a5c65d490266774db3e045c64f5714a767e1
Giri Venkatesan <4477169+gvensan@users.noreply.github.com>
2022-03-23 22:19:15 +0530

    Sample to demonstrate custom `MessageConverter` to handle converting … (#10)
    * Sample to demonstrate custom `MessageConverter` to handle converting null payloads to custom POJOs
    * Updated spring boot version to 2.6.4 to be compatible with spring cloud version 2021.0.1 (bom 2.3.0)
    * Updated version info of spring cloud, spring boot and spring cloud bom on all samples.
    * Added appropriate comments to the null-payload-converter sample and updated methods to generate events with null payload (as spring doesn't allow that)

---
+++ cloud-stream-sink-null-payload-converter/src/main/java/com/solace/samples/spring/scs/NullSensorReadingConverter.java
@@ -0,0 +1,45 @@
+package com.solace.samples.spring.scs;
+
+import org.springframework.messaging.Message;
+import org.springframework.messaging.converter.AbstractMessageConverter;
+
+import com.solace.samples.spring.common.SensorReading;
+import com.solace.spring.cloud.stream.binder.messaging.SolaceBinderHeaders;
+
+public class NullSensorReadingConverter extends AbstractMessageConverter {
+	@Override
+	protected boolean supports(Class<?> clazz) {
+		return SensorReading.class.equals(clazz);
+	}
+
+	@Override
+	protected Object convertFromInternal(Message<?> message, Class<?> targetClass, Object conversionHint) {
+		/*
+		 * NOTE: If the payload is null, then the "solace_scst_nullPayload" message header will be set to true. 
+		 * Because the Spring framework does not allow for a message to have a null payload we need to 
+		 * add a custom message converter to handle that scenario to return a message with either a 
+		 * default or custom payload. 
+		 */
+		
+		if ((boolean) message.getHeaders().getOrDefault(SolaceBinderHeaders.NULL_PAYLOAD, false)) {
+			return new SensorReading();
+		} else {
+			return super.convertFromInternal(message, targetClass, conversionHint);
+		}
+	}
+	
+	/*
+	 * How to test this?
+	 * 
+	 * Spring doesn't allow sending messages with NULL payload. Hence, a unit test can not be 
+	 * successfully built or run. 
+	 * 
+	 * However to test this feature, we can follow any of the following options manually:
+	 * 
+	 * 1. Use of Try Me! tool in the PubSub+ Manager tool
+	 * 		Publish events on the topic with empty message content
+	 * 2. Use sdkperf to publish events on the topic with emptry payload
+	 * 		./sdkperf_java.sh -cip="localhost:55554" -ptl "sensor/temperature/99" -mn 1 -mr 1 -md
+	 */
+
+}
